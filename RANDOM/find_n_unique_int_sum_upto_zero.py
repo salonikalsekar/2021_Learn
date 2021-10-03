279e613f36513a5132c829d0ce984dba8f67b605
Saloni <saloniskalsekar@gmail.com>
2021-10-02 21:23:08 -0400

    random

---
+++ RANDOM/find_n_unique_int_sum_upto_zero.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+
+        if n % 2 == 0:
+            for i in range(1, n // 2 + 1):
+                res.append(i)
+                res.append(-i)
+        else:
+            for i in range(-n // 2 + 1, n // 2 + 1):
+                res.append(i)
+
+        return res
+
+
+
+
